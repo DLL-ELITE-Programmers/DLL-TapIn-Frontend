7c052db91670ba583251ec75bd7df4f48a983b9e
Jemson <jemson.personal@gmail.com>
2025-07-23 23:00:50 +0800

    Updated hero

--- src/pages/hero.tsx
+++ src/pages/hero.tsx
@@ -1,7 +1,6 @@
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { Image, Text, View } from "react-native";
-import Title from "src/component/title";
-import Btn from "src/widgets/button";
+import { Image, Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
 import { RootStackParamList } from "types";
 
 type HeroScreenNavigationProp = NativeStackNavigationProp<
@@ -15,37 +14,52 @@ interface Props {
 
 export default function Hero({ navigation }: Props) {
   return (
-    <View className="flex flex-col w-full flex-1 p-4 items-center justify-center gap-4 pb-6">
-      <View className="w-full gap-4 justify-center items-center flex-1">
-        <View className="w-1/3 aspect-square">
-          <Image
-            className="w-full h-full"
-            source={require("../../assets/dll_logo.png")}
-          />
-        </View>
-        <Title />
-
-        <View className="w-full mt-10">
-          <Btn
-            onclick={() => {
-              navigation.navigate("Login");
-            }}
-          >
-            Login
-          </Btn>
+    <LinearGradient
+      colors={['transparent', '#1E40AF']}
+      start={{ x: 0, y: 0 }} 
+      end={{ x: 0, y: 1 }}
+      className="flex-1"
+    >
+      <View className="flex flex-col w-full flex-1 p-4 items-center justify-end gap-4 mb-10 pb-6">
+        <View className="flex justify-end w-full items-center">
+          <View className="w-3/5 aspect-square">
+            <Image
+              className="w-full h-full"
+              source={require("../../assets/dll_logo.png")}
+            />
+          </View>
+        
+          <View className="w-full items-center mt-8">
+            <Text className="font-semibold text-5xl text-white">DLL - TapIn</Text>
+            <Text className="mt-2 text-white">Automated Attendance System</Text>
+          </View>
+          <View style={{ width: '80%', marginTop: 40 }}>
+            <TouchableOpacity
+              onPress={() => navigation.navigate('Login')}
+              style={{
+                backgroundColor: '#fff',
+                paddingVertical: 12,
+                paddingHorizontal: 24,
+                borderRadius: 25,
+                alignItems: 'center',
+              }}
+            >
+              <Text className="text-base font-semibold">Login</Text>
+            </TouchableOpacity>
+          </View>
+          <View className="flex flex-row items-center justify-center mt-4">
+              <Text className="text-white">Don't have an account? </Text>
+            <Text
+              onPress={() => {
+                navigation.navigate("Signup");
+              }}
+              className="underline font-bold text-white"
+            >
+              Sign up
+            </Text>
+          </View>
         </View>
       </View>
-      <View className="flex flex-row justify-end">
-        <Text>Don't have an account? </Text>
-        <Text
-          onPress={() => {
-            navigation.navigate("Signup");
-          }}
-          className="underline font-bold text-blue-600"
-        >
-          Sign up
-        </Text>
-      </View>
-    </View>
+    </LinearGradient>
   );
 }
