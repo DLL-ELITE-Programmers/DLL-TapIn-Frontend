93ebb20dc93c0cabee2a33245066186be1d0e764
RyannKim327 <rksesgundo123@gmail.com>
2026-06-04 05:49:28 +0800

    06-04-26  5:49 [hindi ko tanda ung updated dito pero more on layout sya]

--- src/pages/pwa/landing.tsx
+++ src/pages/pwa/landing.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import DLLLogo from "./../../assets/dll_logo.png"
+import Logo from "@/assets/logo.png"
 import Login from "./login";
 import Registration from "./register";
 import { ToastContainer } from "react-toastify";
@@ -11,9 +11,9 @@ export default function LandingPage() {
 	return (
 		<div className={`flex flex-col items-center h-full w-full p-6 relative`}>
 			<div className={`flex flex-col items-center w-full justify-center ${login || register ? "h-[calc(50%-1rem)]" : "h-[calc(100%-120px)]"} transition-all duration-500 ease-in-out`}>
-				<img className="w-[35%] md:w-[15%] drop-shadow-2xl" src={DLLLogo} />
+				<img className="w-[35%] md:w-[15%] drop-shadow-2xl rounded-full" src={Logo} />
 				<div className="flex flex-col items-center justify-center text-center w-full gap-2">
-					<span className="oswald text-sky-900 text-[2.5rem] md:text-[4rem] font-bold tracking-tight">DLL Tap IN</span>
+					<span className="oswald text-sky-900 text-[2.5rem] md:text-[4rem] font-bold tracking-tight">DLL VeriScan</span>
 					{
 						login || register ? (
 							<div className="flex flex-col items-center text-center gap-0.5 text-sky-900/60 mt-2 animate-in fade-in duration-700">
--- vite.config.ts
+++ vite.config.ts
@@ -28,34 +28,34 @@ export default defineConfig({
 
       manifest: {
         name: 'DLL Attendance Monitoring',
-        short_name: 'DLL Tap-IN',
+        short_name: 'DLL VeriScan',
         description: 'An attendance monitoring system',
 
         theme_color: '#2563eb',
         background_color: '#ffffff',
         display: 'standalone',
         orientation: 'portrait',
-        start_url: '/',
+        start_url: '/pwa',
         scope: '/',
 
         icons: [
           {
-            src: 'favicon.svg',
-            sizes: '192x192',
-            type: 'image/svg+xml',
+            src: 'logo-96.png',
+            sizes: '96x96',
+            type: 'image/png',
             purpose: 'any'
           },
           {
-            src: 'favicon.svg',
-            sizes: '512x512',
-            type: 'image/svg+xml',
+            src: 'logo-128.png',
+            sizes: '128x128',
+            type: 'image/png',
             purpose: 'any'
           },
           {
-            src: 'favicon.svg',
-            sizes: '512x512',
-            type: 'image/svg+xml',
-            purpose: 'maskable'
+            src: 'logo-256.png',
+            sizes: '256x256',
+            type: 'image/png',
+            purpose: 'any'
           }
         ]
       },
